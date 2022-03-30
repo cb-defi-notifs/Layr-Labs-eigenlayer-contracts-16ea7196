fc8d0ff90f38745cbb3125872bce1d198c9db86f
dragonboy1994 <soubhik@uw.edu>
2022-03-29 20:27:02 -0400

    serious bug

--- src/contracts/investment/AaveInvestmentStrategy.sol
+++ src/contracts/investment/AaveInvestmentStrategy.sol
@@ -23,6 +23,14 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
         _underlyingToken.approve(address(_lendingPool), type(uint256).max);
     }
 
+
+    /**
+     * @notice Used for depositing assets into Aave
+     */
+    /**
+     * @return newShares is the number of shares issued at the current price for each share
+     *         in terms of aToken. 
+     */
     function deposit(
         IERC20 token,
         uint256 amount
@@ -32,6 +40,7 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
         if (token == underlyingToken) {
             //deposit and the "shares" are in proportion to the new aTokens minted
             aTokensBefore = aToken.balanceOf(address(this));
+
             //tokens have already been transferred to this contract
             //underlyingToken.transferFrom(depositor, address(this), amounts[0]);
             lendingPool.deposit(
@@ -40,27 +49,54 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
                 address(this),
                 0
             );
+
+            // increment in the aToken balance of this contract due to the new investment
             aTokenIncrease = aToken.balanceOf(address(this)) - aTokensBefore;
         } else if (token == aToken) {
             aTokenIncrease = amount;
+
+            // total aToken with this contract before the new investment,
+            // this includes interest rates accrued on existing investment
             aTokensBefore = aToken.balanceOf(address(this)) - amount;
         } else {
             revert("can only deposit underlyingToken or aToken");
         }
         if (totalShares == 0) {
+            // no existing investment into this investment strategy
             newShares = aTokenIncrease;
         } else {
+            /**
+             * @dev Evaluating the number of new shares that would be issued for the increase
+             *      in aToken at the current price of each share in terms of aToken. This 
+             *      price is given by aTokensBefore / totalShares.  
+             */
             newShares = (aTokenIncrease * totalShares) / aTokensBefore;
         }
+
+        // incrementing the total number of shares
         totalShares += newShares;
     }
 
+
+    /**
+     * @notice Used for withdrawing assets from Aave in the specified token
+     */
+    /**
+     * @param depositer is the withdrawer's address
+     * @param token is the token in which deposter intends to get back its assets
+     * @param shareAmount is the amount of share that the depositer wants to exchange for 
+     *        withdrawing its assets
+     */
+    /**
+     * @return amountWithdrawn is the amount of specified token that is being withdrawn
+     */ 
     function withdraw(
         address depositor,
         IERC20 token,
         uint256 shareAmount
     ) external onlyInvestmentManager returns(uint256 amountWithdrawn) {
         uint256 toWithdraw = sharesToUnderlying(shareAmount);
+
         if (token == underlyingToken) {
             //withdraw from lendingPool
             amountWithdrawn = lendingPool.withdraw(
@@ -68,7 +104,10 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
                 toWithdraw,
                 depositor
             );
+
+            // transfer the underlyingToken to the depositer
             underlyingToken.transfer(depositor, amountWithdrawn);
+
         } else if (token == aToken) {
             aToken.transfer(depositor, toWithdraw);
             amountWithdrawn = toWithdraw;
@@ -99,6 +138,13 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
             return (aToken.balanceOf(address(this)) * amountShares) / totalShares;
         }
     }
+
+    /**
+     * @notice get a conversion of aToken from the input shares
+     */
+    /**
+     * @param amountShares is the number of shares whose conversion is to be checked
+     */ 
     function sharesToUnderlying(uint256 amountShares) public view returns(uint256) {
         if (totalShares == 0) {
             return 0;
@@ -106,6 +152,8 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
             return (aToken.balanceOf(address(this)) * amountShares) / totalShares;
         }
     }
+
+
     function underlyingToSharesView(uint256 amountUnderlying) public view returns(uint256) {
         if (totalShares == 0) {
             return amountUnderlying;
@@ -113,6 +161,13 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
             return (amountUnderlying * totalShares) / aToken.balanceOf(address(this));
         }
     }
+
+    /**
+     * @notice get a conversion of inout aToken to the shares at current price
+     */
+    /**
+     * @param amountUnderlying is the amount of aToken for which number of shares is to be checked
+     */ 
     function underlyingToShares(uint256 amountUnderlying) public view returns(uint256) {
         if (totalShares == 0) {
             return amountUnderlying;
@@ -120,6 +175,8 @@ abstract contract AaveInvestmentStrategy is Initializable, Governed, AaveInvestm
             return (amountUnderlying * totalShares) / aToken.balanceOf(address(this));
         }
     }
+
+
     function userUnderlying(address user) public view returns(uint256) {
         return sharesToUnderlying(shares(user));
     }
