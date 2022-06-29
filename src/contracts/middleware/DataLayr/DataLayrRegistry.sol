8634c1d1ae08464c8f0d25372506be0239755294
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-06-29 10:37:31 -0700

    remove unused 'onlyRepository' modifier

--- src/contracts/middleware/DataLayr/DataLayrRegistry.sol
+++ src/contracts/middleware/DataLayr/DataLayrRegistry.sol
@@ -183,12 +183,6 @@ contract DataLayrRegistry is
         address registrant
     );
 
-    // MODIFIERS
-    modifier onlyRepository() {
-        require(address(repository) == msg.sender, "onlyRepository");
-        _;
-    }
-
 
     constructor(
         Repository _repository,
