6a27d4ccd948ecf4f1e26b29415f799dec5077d7
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-08-04 15:14:29 -0700

    improved comments

--- src/contracts/middleware/Repository.sol
+++ src/contracts/middleware/Repository.sol
@@ -8,25 +8,39 @@ import "../interfaces/IRepository.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
 
+    /*
+     The Repository contract is intended to be the central source of "ground truth" for
+     a single middleware on EigenLayr.
+     Other contracts can refer to the Repository in order to determine the "official"
+     contracts for the middleware, making it the central point for upgrades-by-changing-contract-addresses.
+     The owner of the Repository for a middleware holds *tremendous power* – this role
+     should only be given to a multisig or governance contract.
+    */
 contract Repository is Ownable, Initializable, IRepository {
+    // address of the Delegation contract of EigenLayr
     IEigenLayrDelegation public immutable delegation;
+    // address of the InvestmentManager contract of EigenLayr
     IInvestmentManager public immutable investmentManager;
+    // the VoteWeigher contract for this middleware, which determines quorum weighing functions
     IVoteWeigher public voteWeigher;
+    // the Registry contract for this middleware, where operators register and deregister
     IRegistry public registry;
+    // the ServiceManager contract for this middleware, where tasks are created / initiated
     IServiceManager public serviceManager;
 
+    // set the (immutable) `delegation` and `investmentManager` addresses -- these are global to EigenLayr and should not change
     constructor (IEigenLayrDelegation _delegation, IInvestmentManager _investmentManager) {
         delegation = _delegation;
         investmentManager = _investmentManager;
     }
 
-    /// @notice returns the owner of the repository contract
+    /// @notice returns the owner of this repository contract
     function owner() public view override(Ownable, IRepository) returns (address) {
         return Ownable.owner();
     }
 
     /**
-     @notice used for setting the associated contracts for the middleware.
+     @notice initializer (callable only once). Used for setting the associated contracts for the middleware and owner of this contract.
      */
     function initialize(
         IVoteWeigher _voteWeigher,
@@ -40,17 +54,17 @@ contract Repository is Ownable, Initializable, IRepository {
         _transferOwnership(initialOwner);
     }
 
-    /// @notice sets the service manager for the middleware's repository
+    /// @notice sets the ServiceManager for the middleware
     function setServiceManager(IServiceManager _serviceManager) external onlyOwner {
         serviceManager = _serviceManager;
     }
 
-    /// @notice sets the Registry for the middleware's repository
+    /// @notice sets the Registry for the middleware
     function setRegistry(IRegistry _registry) external onlyOwner {
         registry = _registry;
     }
 
-    /// @notice sets the vote weigher for the middleware's repository
+    /// @notice sets the vote weigher for the middleware
     function setVoteWeigher(IVoteWeigher _voteWeigher) external onlyOwner {
         voteWeigher = _voteWeigher;
     }
