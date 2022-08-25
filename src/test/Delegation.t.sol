9f300e3269d42302d2a0fbb1a698fec7f3eb5093
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-08-25 10:25:30 -0700

    add assumption to fuzzed input of flaky test

--- src/test/Delegation.t.sol
+++ src/test/Delegation.t.sol
@@ -405,6 +405,7 @@ contract Delegator is EigenLayrDeployer {
     ///         setUndelegationFraudProofInterval functions.
 
     function testOwnableFunctions(address badGuy) fuzzedAddress(badGuy) public {
+        cheats.assume(badGuy != delegation.owner());
         cheats.startPrank(badGuy);
         EigenLayrDelegation altDelegation = new EigenLayrDelegation();
         IInvestmentManager altInvestmentManager = new InvestmentManager(altDelegation);
