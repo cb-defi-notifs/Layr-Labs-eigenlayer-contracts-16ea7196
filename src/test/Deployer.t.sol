7d5b1b38bba362ab162faa5df41e18c66da05c20
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-08-25 15:24:36 -0700

    significant code de-duplication

--- src/test/Deployer.t.sol
+++ src/test/Deployer.t.sol
@@ -599,30 +599,38 @@ contract EigenLayrDeployer is
     }
 
     // second return value is the complete `searchData` that can serve as an input to `stakeWithdrawalVerification`
-    function _testConfirmDataStoreSelfOperators(uint8 signersInput) 
+    function _testConfirmDataStoreSelfOperators(uint8 numSigners) 
         internal 
         returns (bytes memory, IDataLayrServiceManager.DataStoreSearchData memory)
         {
-        cheats.assume(signersInput > 0 && signersInput <= 15);
-
-        uint32 numberOfSigners = uint32(signersInput);
+        cheats.assume(numSigners > 0 && numSigners <= 15);
 
         //register all the operators
-        for (uint256 i = 0; i < numberOfSigners; ++i) {
+        for (uint256 i = 0; i < numSigners; ++i) {
             _testRegisterAdditionalSelfOperator(
                 signers[i],
                 registrationData[i]
             );
         }
 
+        // hard-coded value
+        uint256 index = 0;
+
+        return _testConfirmDataStoreWithoutRegister(index, numSigners);
+    }
+
+    function _testConfirmDataStoreWithoutRegister(uint256 index, uint8 numSigners) 
+        internal 
+        returns (bytes memory, IDataLayrServiceManager.DataStoreSearchData memory)
+    {
         uint256 initTime = 1000000001;
         IDataLayrServiceManager.DataStoreSearchData memory searchData = _testInitDataStore(initTime, address(this));
 
         uint32 numberOfNonSigners = 0;
-        (uint256 apk_0, uint256 apk_1, uint256 apk_2, uint256 apk_3) = getAggregatePublicKey(uint256(numberOfSigners));
+        (uint256 apk_0, uint256 apk_1, uint256 apk_2, uint256 apk_3) = getAggregatePublicKey(uint256(numSigners));
+        (uint256 sigma_0, uint256 sigma_1) = getSignature(uint256(numSigners), index);//(signatureData[index*2], signatureData[2*index + 1]);
 
-        (uint256 sigma_0, uint256 sigma_1) = getSignature(uint256(numberOfSigners), 0);//(signatureData[0], signatureData[1]);
-        
+
         /** 
      @param data This calldata is of the format:
             <
@@ -632,71 +640,6 @@ contract EigenLayrDeployer is
              uint32 dataStoreId
              uint32 numberOfNonSigners,
              uint256[numberOfNonSigners][4] pubkeys of nonsigners,
-             uint32 apkIndex,
-             uint256[4] apk,
-             uint256[2] sigma
-            >
-     */
-        // 
-        bytes memory data = abi.encodePacked(
-            keccak256(abi.encodePacked(searchData.metadata.globalDataStoreId, searchData.metadata.headerHash, searchData.duration, initTime, uint32(0))),
-            uint48(dlReg.getLengthOfTotalStakeHistory() - 1),
-            searchData.metadata.blockNumber,
-            searchData.metadata.globalDataStoreId,
-            numberOfNonSigners,
-            // no pubkeys here since zero nonSigners for now
-            uint32(dlReg.getApkUpdatesLength() - 1),
-            apk_0,
-            apk_1,
-            apk_2,
-            apk_3,
-            sigma_0,
-            sigma_1
-        );
-        
-        bytes32 signatoryRecordHash;
-        (
-            // uint32 dataStoreIdToConfirm,
-            // uint32 blockNumberFromTaskHash,
-            // bytes32 msgHash,
-            // SignatoryTotals memory signedTotals,
-            // bytes32 signatoryRecordHash
-            ,
-            ,
-            ,
-            ,
-            signatoryRecordHash
-        ) = dlsm.checkSignatures(data);
-
-        dlsm.confirmDataStore(data, searchData);
-        cheats.stopPrank();
-
-        // copy the signatoryRecordHash to the `searchData` struct, so the `searchData` can now be used in `stakeWithdrawalVerification` calls appropriately
-        searchData.metadata.signatoryRecordHash = signatoryRecordHash;
-
-        return (data, searchData);
-    }
-
-
-    function _testConfirmDataStoreWithoutRegister(uint index, uint256 numSigners) internal {
-        uint256 initTime = 1000000001;
-        IDataLayrServiceManager.DataStoreSearchData
-            memory searchData = _testInitDataStore(initTime, address(this));
-
-        uint32 numberOfNonSigners = 0;
-        (uint256 apk_0, uint256 apk_1, uint256 apk_2, uint256 apk_3) = getAggregatePublicKey(numSigners);
-        (uint256 sigma_0, uint256 sigma_1) = getSignature(numSigners, index);//(signatureData[index*2], signatureData[2*index + 1]);
-
-
-        /** 
-     @param data This calldata is of the format:
-            <
-             bytes32 msgHash,
-             uint48 index of the totalStake corresponding to the dataStoreId in the 'totalStakeHistory' array of the BLSRegistryWithBomb
-             uint32 blockNumber
-             uint32 dataStoreId
-             uint32 numberOfNonSigners,
-             uint256[numberOfSigners][4] pubkeys of nonsigners,
              uint32 apkIndex,
              uint256[4] apk,
              uint256[2] sigma
@@ -722,15 +665,37 @@ contract EigenLayrDeployer is
             sigma_1
         );
 
+        // get the signatoryRecordHash that will result from the `confirmDataStore` call (this is used in modifying the dataStoreHash post-confirmation)
+        bytes32 signatoryRecordHash;
+        (
+            // uint32 dataStoreIdToConfirm,
+            // uint32 blockNumberFromTaskHash,
+            // bytes32 msgHash,
+            // SignatoryTotals memory signedTotals,
+            // bytes32 signatoryRecordHash
+            ,
+            ,
+            ,
+            ,
+            signatoryRecordHash
+        ) = dlsm.checkSignatures(data);
+
         uint256 gasbefore = gasleft();
         dlsm.confirmDataStore(data, searchData);
         emit log_named_uint("confirm gas overall", gasbefore - gasleft());
-
+        cheats.stopPrank();
         // bytes32 sighash = dlsm.getDataStoreIdSignatureHash(
         //     dlsm.dataStoreId() - 1
         // );
         // assertTrue(sighash != bytes32(0), "Data store not committed");
-        cheats.stopPrank();
+
+        /**
+         * Copy the signatoryRecordHash to the `searchData` struct, so the `searchData` can now be used in `stakeWithdrawalVerification` calls appropriately
+         * This must be done *after* the call to `dlsm.confirmDataStore`, since the appropriate `searchData` changes as a result of this call
+         */
+        searchData.metadata.signatoryRecordHash = signatoryRecordHash;
+
+        return (data, searchData);
     }
 
     // simply tries to register 'sender' as a delegate, setting their 'DelegationTerms' contract in EigenLayrDelegation to 'dt'
@@ -880,6 +845,7 @@ contract EigenLayrDeployer is
         return
             uint256(totalBytes * feePerBytePerTime * duration * DURATION_SCALE);
     }
+
     function testDeploymentSuccessful() public {
         // assertTrue(address(eigen) != address(0), "eigen failed to deploy");
         assertTrue(
@@ -912,6 +878,4 @@ contract EigenLayrDeployer is
         );
 
     }
-
-
 }
