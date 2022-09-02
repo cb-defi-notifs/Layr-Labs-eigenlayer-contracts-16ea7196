e78bc7a01547d7d5cb2a5b9d24ae63e84e41c88c
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-09-02 09:22:58 -0700

    InvestmentManager storage cleanup
    Delete deprecated variables

--- src/contracts/investment/InvestmentManagerStorage.sol
+++ src/contracts/investment/InvestmentManagerStorage.sol
@@ -11,26 +11,21 @@ abstract contract InvestmentManagerStorage is IInvestmentManager {
     // TODO: set this to a proper interval for production
     uint32 public constant WITHDRAWAL_WAITING_PERIOD = 10 seconds;
 
-    // maximum length of dynamic arrays in `investorStrats` mapping
+    // maximum length of dynamic arrays in `investorStrats` mapping, for sanity's sake
     uint8 internal constant MAX_INVESTOR_STRATS_LENGTH = 32;
 
+    // system contracts
     IEigenLayrDelegation public immutable delegation;
     ISlasher public slasher;
 
-    IInvestmentStrategy public proofOfStakingEthStrat;
-    IInvestmentStrategy public consensusLayerEthStrat;
-
-    uint256 public totalEigenStaked;
-    address public eigenLayrDepositContract;
-    // staker => InvestmentStrategy => num shares
-    mapping(address => mapping(IInvestmentStrategy => uint256))
-        public investorStratShares;
+    // staker => InvestmentStrategy => number of shares which they currently hold
+    mapping(address => mapping(IInvestmentStrategy => uint256)) public investorStratShares;
     // staker => array of strategies in which they have nonzero shares
     mapping(address => IInvestmentStrategy[]) public investorStrats;
     // staker => hash of withdrawal inputs => timestamps & address related to the withdrawal
     mapping(address => mapping(bytes32 => WithdrawalStorage)) public queuedWithdrawals;
     // staker => cumulative number of queued withdrawals they have ever initiated. only increments (doesn't decrement)
-    mapping(address => uint96) public numWithdrawalsQueued;
+    mapping(address => uint256) public numWithdrawalsQueued;
 
     constructor(IEigenLayrDelegation _delegation) {
         delegation = _delegation;
