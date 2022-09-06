6f0b9905e70c602a2780b4eb4de03a93bfdcb193
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-09-06 14:33:29 -0700

    comment out the `ServiceManagerBase` contract
    This contract is not in a great state. In all likelihood we should delete it, but for now I'm just commenting it out.

--- src/contracts/middleware/ServiceManagerStorage.sol
+++ src/contracts/middleware/ServiceManagerStorage.sol
@@ -1,92 +1,92 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../interfaces/IRepository.sol";
-import "../interfaces/IServiceManager.sol";
-import "../interfaces/IRegistry.sol";
-import "../interfaces/IEigenLayrDelegation.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "../interfaces/IRepository.sol";
+// import "../interfaces/IServiceManager.sol";
+// import "../interfaces/IRegistry.sol";
+// import "../interfaces/IEigenLayrDelegation.sol";
 
-abstract contract ServiceManagerStorage is IServiceManager {
+// abstract contract ServiceManagerStorage is IServiceManager {
     
-    // DATA STRUCTURE
+//     // DATA STRUCTURE
 
-    /**
-     @notice used for storing information on the most recent payment made to the operator
-     */
-    struct Payment {
-        // taskNumber starting from which payment is being claimed 
-        uint32 fromTaskNumber; 
+//     /**
+//      @notice used for storing information on the most recent payment made to the operator
+//      */
+//     struct Payment {
+//         // taskNumber starting from which payment is being claimed 
+//         uint32 fromTaskNumber; 
 
-        // taskNumber until which payment is being claimed (exclusive) 
-        uint32 toTaskNumber; 
+//         // taskNumber until which payment is being claimed (exclusive) 
+//         uint32 toTaskNumber; 
 
-        // recording when committment for payment made; used for fraud proof period
-        uint32 commitTime; 
+//         // recording when committment for payment made; used for fraud proof period
+//         uint32 commitTime; 
 
-        // payment for range [fromTaskNumber, toTaskNumber)
-        /// @dev max 1.3e36, keep in mind for token decimals
-        uint120 amount; 
-
-
-        uint8 status; // 0: committed, 1: redeemed
-        uint256 collateral; //account for if collateral changed
-    }
-
-    /**
-     * @notice the ERC20 token that will be used by the disperser to pay the service fees to
-     *         nodes.
-     */
-    IERC20 public immutable paymentToken;
-
-    IERC20 public immutable collateralToken;
-
-    /**
-     * @notice service fee that will be paid out by the disperser to the nodes
-     *         for storing per byte for per unit time. 
-     */
-    uint256 public feePerBytePerTime;
-
-    /**
-     * @notice challenge window for submitting fraudproof in case of incorrect payment 
-     *         claim by the registered operator 
-     */
-    uint256 public constant paymentFraudProofInterval = 7 days;
+//         // payment for range [fromTaskNumber, toTaskNumber)
+//         /// @dev max 1.3e36, keep in mind for token decimals
+//         uint120 amount; 
 
 
-    /**
-     @notice this is the payment that has to be made as a collateral for fraudproof 
-             during payment challenges
-     */
-    uint256 public paymentFraudProofCollateral = 1 wei;
+//         uint8 status; // 0: committed, 1: redeemed
+//         uint256 collateral; //account for if collateral changed
+//     }
+
+//     /**
+//      * @notice the ERC20 token that will be used by the disperser to pay the service fees to
+//      *         nodes.
+//      */
+//     IERC20 public immutable paymentToken;
+
+//     IERC20 public immutable collateralToken;
+
+//     /**
+//      * @notice service fee that will be paid out by the disperser to the nodes
+//      *         for storing per byte for per unit time. 
+//      */
+//     uint256 public feePerBytePerTime;
+
+//     /**
+//      * @notice challenge window for submitting fraudproof in case of incorrect payment 
+//      *         claim by the registered operator 
+//      */
+//     uint256 public constant paymentFraudProofInterval = 7 days;
 
 
-    /// @notice counter for number of assertions of data that has happened on this middleware
-    uint32 public taskNumber = 1;
+//     /**
+//      @notice this is the payment that has to be made as a collateral for fraudproof 
+//              during payment challenges
+//      */
+//     uint256 public paymentFraudProofCollateral = 1 wei;
 
-    /**
-     * @notice mapping between the taskNumber for a particular assertion 
-     *         and a compressed information on the signatures of the 
-     *         nodes who signed up to be the part of the quorum.  
-     */
-    mapping(uint32 => bytes32) public taskNumberToSignatureHash;
 
-    /**
-     * @notice mapping between the total service fee that would be paid out in the 
-     *         corresponding assertion
-     */
-    mapping(uint32 => uint256) public taskNumberToFee;
+//     /// @notice counter for number of assertions of data that has happened on this middleware
+//     uint32 public taskNumber = 1;
 
-    /**
-     * @notice mapping between the operator and its current committed or payment
-     *         or the last redeemed payment 
-     */
-    mapping(address => Payment) public operatorToPayment;
+//     /**
+//      * @notice mapping between the taskNumber for a particular assertion 
+//      *         and a compressed information on the signatures of the 
+//      *         nodes who signed up to be the part of the quorum.  
+//      */
+//     mapping(uint32 => bytes32) public taskNumberToSignatureHash;
 
-    mapping(address => address) public operatorToPaymentChallenge;
+//     /**
+//      * @notice mapping between the total service fee that would be paid out in the 
+//      *         corresponding assertion
+//      */
+//     mapping(uint32 => uint256) public taskNumberToFee;
+
+//     /**
+//      * @notice mapping between the operator and its current committed or payment
+//      *         or the last redeemed payment 
+//      */
+//     mapping(address => Payment) public operatorToPayment;
+
+//     mapping(address => address) public operatorToPaymentChallenge;
  
-    constructor(IERC20 _paymentToken, IERC20 _collateralToken) {
-        paymentToken = _paymentToken;
-        collateralToken = _collateralToken;
-    }
-}
+//     constructor(IERC20 _paymentToken, IERC20 _collateralToken) {
+//         paymentToken = _paymentToken;
+//         collateralToken = _collateralToken;
+//     }
+// }
