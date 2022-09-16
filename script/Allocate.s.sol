a95f437a7fa34ebb865191b2b882359381b5feb9
gpsanant <gpsanant@gmail.com>
2022-09-15 17:55:10 -0700

    script compiles

--- script/Allocate.s.sol
+++ script/Allocate.s.sol
@@ -8,8 +8,6 @@ contract Allocate is
     DSTest,
     EigenLayerParser
 {
-    using BytesLib for bytes;
-
     //performs basic deployment before each test
     function run() external {
         // read meta data from json
@@ -20,7 +18,6 @@ contract Allocate is
         // deployer allocate weth, eigen to staker
         for (uint i = 0; i < numStaker ; ++i) {
             address stakerAddr = stdJson.readAddress(configJson, string.concat(".staker[", string.concat(vm.toString(i), "].address")));
-            weth.balanceOf(address(this));
             weth.transfer(stakerAddr, wethAmount);
             eigen.transfer(stakerAddr, wethAmount);
             emit log("stakerAddr");
@@ -37,3 +34,30 @@ contract Allocate is
         vm.stopBroadcast();
     }
 }
+
+contract ProvisionWeth is
+    Script,
+    DSTest,
+    EigenLayerParser
+{
+    uint256 wethAmount = 100000000000000000000;
+    //performs basic deployment before each test
+    function run() external {
+        vm.startBroadcast();
+        // read meta data from json
+        addressJson = vm.readFile("contract.addresses");
+        weth = IERC20(stdJson.readAddress(addressJson, ".weth"));
+        address dlsm = stdJson.readAddress(addressJson, ".dlsm");
+        // deployer allocate weth, eigen to disperser
+        configJson = vm.readFile("recipient.json");
+        uint256 recipientPrivKey = stdJson.readUint(configJson, string.concat(".private"));
+        address recipientAddr = cheats.addr(recipientPrivKey);        
+        weth.transfer(recipientAddr, wethAmount);
+        payable(recipientAddr).transfer(1 ether);
+        vm.stopBroadcast();
+        //approve dlsm
+        vm.broadcast(recipientPrivKey);
+        weth.approve(dlsm, type(uint256).max);
+    }
+}
+
