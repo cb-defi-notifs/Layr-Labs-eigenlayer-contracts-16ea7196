aa3bc0d5fc58082d9d46b17e2b53b1b2ebde91ea
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-10-10 12:09:41 -0700

    more NatSpec

--- src/contracts/interfaces/IBLSPublicKeyCompendium.sol
+++ src/contracts/interfaces/IBLSPublicKeyCompendium.sol
@@ -6,8 +6,12 @@ pragma solidity ^0.8.9;
  * @author Layr Labs, Inc.
  */
 interface IBLSPublicKeyCompendium {
+    /// @notice mapping from operator addresss to pubkey hash
     function operatorToPubkeyHash(address operator) external view returns (bytes32);
+
+    /// @notice mapping from pubkey hash to operator addresss
     function pubkeyHashToOperator(bytes32 pubkeyHash) external view returns (address);
+
     /**
      * @notice Called by an operator to register themselves as the owner of a BLS public key.
      * @param data is the calldata that contains the coordinates for pubkey on G2 and signature on G1.
