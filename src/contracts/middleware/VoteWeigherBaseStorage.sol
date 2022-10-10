5b72c966d0c17e4270ebd82c05df7e86592f7153
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-10-10 14:50:51 -0700

    fix typo in comment

--- src/contracts/middleware/VoteWeigherBaseStorage.sol
+++ src/contracts/middleware/VoteWeigherBaseStorage.sol
@@ -42,7 +42,7 @@ abstract contract VoteWeigherBaseStorage is RepositoryAccess, IVoteWeigher {
     mapping(uint256 => StrategyAndWeightingMultiplier[]) public strategiesConsideredAndMultipliers;
     /**
      * @notice This defines the earnings split between different quorums. Mapping is quorumNumber => BIPS which the quorum earns, out of the total earnings.
-     * @notice The sum of all entries, i.e. sum(quorumBips[0] through quorumBips[NUMBER_OF_QUORUMS]) should *always* be 10,000!
+     * @notice The sum of all entries, i.e. sum(quorumBips[0] through quorumBips[NUMBER_OF_QUORUMS - 1]) should *always* be 10,000!
      */
     mapping(uint256 => uint256) public quorumBips;
 
