85fc3b5c13e30ae3974edd05288784ce5e0246b0
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-10-26 16:20:03 -0700

    write simple 'library' (technically abstract contract) for spare merkle proofs

--- src/contracts/operators/DelegationTerms_Merkle.sol
+++ src/contracts/operators/DelegationTerms_Merkle.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "../interfaces/IDelegationTerms.sol";
+import "../libraries/SparseMerkle.sol";
 
 /**
  * @title A 'Delegation Terms' contract that an operator can use to distribute earnings to stakers by posting Merkle roots
@@ -20,8 +21,10 @@ import "../interfaces/IDelegationTerms.sol";
  * to contain the **cumulative** earnings of a staker. This will reduce the total number of actions that stakers who claim only rarely
  * have to take, while allowing stakers to claim their earnings as often as new Merkle roots are posted.
  */
-contract DelegationTerms_Merkle is Ownable, IDelegationTerms {
+contract DelegationTerms_Merkle is SparseMerkle, Ownable, IDelegationTerms {
     using SafeERC20 for IERC20;
+
+    // spare Merkle tree functionality
 
     // staker => token => cumulative amount *claimed*
     mapping(address => mapping(IERC20 => uint256)) public cumulativeClaimedByStakerOfToken;
@@ -30,6 +33,10 @@ contract DelegationTerms_Merkle is Ownable, IDelegationTerms {
     bytes32[] public merkleRoots;
 
     event NewMerkleRootPosted(bytes32 newRoot);
+
+    constructor(uint256 _TREE_DEPTH)
+        SparseMerkle(_TREE_DEPTH)
+    {}
 
     /**
      * @notice Used by the operator to withdraw tokens directly from this contract.
