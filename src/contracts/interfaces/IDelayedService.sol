f26126f84363538188782246414c3443d9557b05
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2022-11-08 11:08:59 -0800

    add NatSpec to IDelayedService

--- src/contracts/interfaces/IDelayedService.sol
+++ src/contracts/interfaces/IDelayedService.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/**
+ * @title Interface for a middleware / service that may look at past stake amounts.
+ * @author Layr Labs, Inc.
+ * @notice Specifically, this interface is designed for services that consult stake amounts up to `BLOCK_STALE_MEASURE`
+ * blocks in the past. This may be necessary due to, e.g., network processing & communication delays, or to avoid race conditions
+ * that could be present with coordinating aggregate operator signatures while service operators are registering & de-registering.
+ */
 interface IDelayedService {
+    /// @notice The maximum amount of blocks in the past that the service will consider stake amounts to still be 'valid'.
     function BLOCK_STALE_MEASURE() external view returns(uint32);    
 }
