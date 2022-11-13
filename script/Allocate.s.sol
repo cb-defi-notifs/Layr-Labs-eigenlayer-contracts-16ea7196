16d2dc03aad08a98445d8801b1cfadd11ea9be1c
gpsanant <gpsanant@gmail.com>
2022-11-13 01:01:52 -0800

    move tx logic to contract

--- script/Allocate.s.sol
+++ script/Allocate.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./utils/Allocator.sol";
 import "./EigenLayerParser.sol";
 
 contract Allocate is
@@ -15,21 +16,29 @@ contract Allocate is
 
         uint256 wethAmount = eigenTotalSupply / (numStaker + numDis + 50); // save 100 portions
         vm.startBroadcast();
+
+        Allocator allocator = new Allocator();
+
+        address[] memory stakers = new address[](numStaker);
         // deployer allocate weth, eigen to staker
         for (uint i = 0; i < numStaker ; ++i) {
             address stakerAddr = stdJson.readAddress(configJson, string.concat(".staker[", string.concat(vm.toString(i), "].address")));
-            weth.transfer(stakerAddr, wethAmount);
-            eigen.transfer(stakerAddr, wethAmount);
+            stakers[i] = stakerAddr;
             emit log("stakerAddr");
             emit log_address(stakerAddr);
         }
+        allocator.allocate(weth, stakers, wethAmount);
+        allocator.allocate(eigen, stakers, wethAmount);
+
+        address[] memory dispersers = new address[](numDis);
         // deployer allocate weth, eigen to disperser
         for (uint i = 0; i < numDis ; ++i) {
             address disAddr = stdJson.readAddress(configJson, string.concat(".dis[", string.concat(vm.toString(i), "].address")));    
-            weth.transfer(disAddr, wethAmount);
+            dispersers[i] = disAddr;
             emit log("disAddr");
             emit log_address(disAddr);
         }
+        allocator.allocate(weth, dispersers, wethAmount);
 
         vm.stopBroadcast();
     }
---
+++ script/utils/Allocator.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Allocator {
+
+    function allocate(IERC20 token, address[] memory recipients, uint256 amount) public {
+        token.transferFrom(msg.sender, address(this), recipients.length * amount);
+        for (uint i = 0; i < recipients.length; i++) {
+            token.transfer(recipients[i], amount);
+        }
+    }
+}
