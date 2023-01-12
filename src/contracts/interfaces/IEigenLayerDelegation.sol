0af72c20432155104fae13ab586acad640762442
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-01-12 00:33:07 +0000

    add eip 1271 compatibility to delegation contracts. this is breaks tests, will fix

--- src/contracts/interfaces/IEigenLayerDelegation.sol
+++ src/contracts/interfaces/IEigenLayerDelegation.sol
@@ -31,9 +31,11 @@ interface IEigenLayerDelegation {
 
     /**
      * @notice Delegates from `staker` to `operator`.
-     * @dev requires that r, vs are a valid ECSDA signature from `staker` indicating their intention for this action
+     * @dev requires that:
+     * 1) if `staker` is an EOA, then `signature` is valid ECSDA signature from `staker`, indicating their intention for this action
+     * 2) if `staker` is a contract, then `signature` must will be checked according to EIP-1271
      */
-    function delegateToBySignature(address staker, address operator, uint256 expiry, bytes32 r, bytes32 vs) external;
+    function delegateToBySignature(address staker, address operator, uint256 expiry, bytes calldata signature) external;
 
     /**
      * @notice Undelegates `staker` from the operator who they are delegated to.
