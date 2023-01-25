558feba2153321bbe3ab71a4570685d146e230c9
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-01-25 15:32:57 -0800

    update Certora scripts with correct compiler version
    0.8.9 => 0.8.12

--- certora/scripts/permissions/verifyPausable.sh
+++ certora/scripts/permissions/verifyPausable.sh
@@ -3,7 +3,7 @@ then
     RULE="--rule $2"
 fi
 
-solc-select use 0.8.9    
+solc-select use 0.8.12
 
 certoraRun certora/harnesses/PausableHarness.sol \
     certora/munged/permissions/PauserRegistry.sol \
