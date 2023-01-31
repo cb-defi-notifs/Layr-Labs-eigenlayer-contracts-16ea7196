150014ae0e5cabe755372c3f0f56c6618dea890c
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-01-31 15:21:27 -0800

    Initial Accounting Fix
    This set of changes significantly simplifies the accounting in EigenPods.
    Key changes are to the EigenPod and InvestmentManager contracts. EigenPodManager also has small changes due to eliminating the need for a couple functions. This commit includes documentation changes, but the docs will need more subsequent improvement + clarification.
    
    Rather than charging "penalties" when podOwners are "overcommitted", instead we now just pull their shares from EigenLayer until the validator has withdrawn. This solution is far friendlier to stakers, while being what seems to be an acceptable tradeoff for middlewares.
    Specifically, all penalty-related logic has been removed from the `verifyOvercommittedStake` function, but it continues to decrement the user's beaconChainETH shares through a call to `eigenPodManager.recordOvercommittedBeaconChainETH`.
    
    There are now 4 cases for a withdrawing validator:
    1) happy path (not proven overcommitted, not overcommitted when withdrawing) -- this works as before (except without penalties) -- REQUIRED_BALANCE_WEI is held for normal withdrawal path, everything in excess becomes instantly withdrawable
    2) overcommitted, but not previously proven to be overcommitted -- the full amount is held for the normal withdrawal path, and all excess shares are removed from EigenLayer
    3) previously proven overcommitted, but no longer overcommitted -- just like (1) above, except the podOwner's beaconChainShares are increased by REQUIRED_BALANCE_WEI (to counterbalance the decrease when they were proven to be overcommitted)
    4) previously proven overcommitted, still overcommitted -- the full amount is held for the normal withdrawal path, and the podOwner is credited with enough beaconChainETH shares in EigenLayer to complete the normal withdrawal process
    
    Note as well the change to InvestmentManager -- the introduction of the `beaconChainETHWithdrawalDebt` mapping helps deal with a potential accounting issue when attempting to decrement a staker's beaconChainETH shares.

--- src/contracts/core/InvestmentManager.sol
+++ src/contracts/core/InvestmentManager.sol
@@ -162,8 +162,7 @@ contract InvestmentManager is
     }
 
     /**
-     * @notice Records an overcommitment event on behalf of a staker. The staker's beaconChainETH shares are decremented by `amount` and the 
-     * EigenPodManager will subsequently impose a penalty upon the staker.
+     * @notice Records an overcommitment event on behalf of a staker. The staker's beaconChainETH shares are decremented by `amount`.
      * @param overcommittedPodOwner is the pod owner to be slashed
      * @param beaconChainETHStrategyIndex is the index of the beaconChainETHStrategy in case it must be removed,
      * @param amount is the amount to decrement the slashedAddress's beaconChainETHStrategy shares
@@ -174,8 +173,18 @@ contract InvestmentManager is
         onlyEigenPodManager
         nonReentrant
     {
+        // get `overcommittedPodOwner`'s shares in the enshrined beacon chain ETH strategy
+        uint256 userShares = investorStratShares[overcommittedPodOwner][beaconChainETHStrategy];
+        // if the amount exceeds the user's shares, then record it as "debt", to be "paid off" when the user completes a withdrawal
+        if (amount > userShares) {
+            uint256 debt = amount - userShares;
+            beaconChainETHWithdrawalDebt[overcommittedPodOwner] += debt;
+            amount -= debt;
+        }
         // removes shares for the enshrined beacon chain ETH strategy
-        _removeShares(overcommittedPodOwner, beaconChainETHStrategyIndex, beaconChainETHStrategy, amount);
+        if (amount != 0) {
+            _removeShares(overcommittedPodOwner, beaconChainETHStrategyIndex, beaconChainETHStrategy, amount);            
+        }
         // create array wrappers for call to EigenLayerDelegation
         IInvestmentStrategy[] memory strategies = new IInvestmentStrategy[](1);
         strategies[0] = beaconChainETHStrategy;
@@ -443,7 +452,7 @@ contract InvestmentManager is
                 if (queuedWithdrawal.strategies[i] == beaconChainETHStrategy) {
 
                     // if the strategy is the beaconchaineth strat, then withdraw through the EigenPod flow
-                    eigenPodManager.withdrawRestakedBeaconChainETH(queuedWithdrawal.depositor, msg.sender, queuedWithdrawal.shares[i]);
+                    _withdrawBeaconChainETH(queuedWithdrawal.depositor, msg.sender, queuedWithdrawal.shares[i]);
                 } else {
                     // tell the strategy to send the appropriate amount of funds to the depositor
                     queuedWithdrawal.strategies[i].withdraw(
@@ -504,7 +513,7 @@ contract InvestmentManager is
 
             if (strategies[i] == beaconChainETHStrategy){
                  //withdraw the beaconChainETH to the recipient
-                eigenPodManager.withdrawRestakedBeaconChainETH(slashedAddress, recipient, shareAmounts[i]);
+                _withdrawBeaconChainETH(slashedAddress, recipient, shareAmounts[i]);
             }
             else{
                 // withdraw the shares and send funds to the recipient
@@ -564,7 +573,7 @@ contract InvestmentManager is
             } else {
                 if (queuedWithdrawal.strategies[i] == beaconChainETHStrategy){
                      //withdraw the beaconChainETH to the recipient
-                    eigenPodManager.withdrawRestakedBeaconChainETH(queuedWithdrawal.depositor, recipient, queuedWithdrawal.shares[i]);
+                    _withdrawBeaconChainETH(queuedWithdrawal.depositor, recipient, queuedWithdrawal.shares[i]);
                 } else {
                     // tell the strategy to send the appropriate amount of funds to the recipient
                     queuedWithdrawal.strategies[i].withdraw(recipient, tokens[i], queuedWithdrawal.shares[i]);
@@ -735,6 +744,27 @@ contract InvestmentManager is
     function _undelegate(address depositor) internal {
         require(investorStrats[depositor].length == 0, "InvestmentManager._undelegate: depositor has active deposits");
         delegation.undelegate(depositor);
+    }
+
+    /*
+     * @notice Withdraws `amount` of virtual 'beaconChainETH' shares from `staker`, with any succesfully withdrawn funds going to `recipient`.
+     * @dev First, the amount is drawn-down by any applicable 'beaconChainETHWithdrawalDebt' that the staker has, before passing any remaining
+     * amount (if applicable) onto a call to the `eigenPodManager.withdrawRestakedBeaconChainETH` function.
+    */
+    function _withdrawBeaconChainETH(address staker, address recipient, uint256 amount) internal {
+        uint256 debt = beaconChainETHWithdrawalDebt[staker];
+        if (debt != 0) {
+            if (amount > debt) {
+                beaconChainETHWithdrawalDebt[staker] = 0;
+                amount -= debt;
+            } else {
+                beaconChainETHWithdrawalDebt[staker] = (debt - amount);
+                // rather than setting amount to 0, just return early
+                return;
+            }
+        }
+        // withdraw the beaconChainETH to the recipient
+        eigenPodManager.withdrawRestakedBeaconChainETH(staker, recipient, amount);
     }
 
     // VIEW FUNCTIONS
