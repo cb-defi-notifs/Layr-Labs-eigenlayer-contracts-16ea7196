59e12d9784eb888f6fd004419d73492b1748c66a
Siddhartha Jagannath <60323455+Sidu28@users.noreply.github.com>
2023-02-15 18:43:00 -0500

    added V1 contracts

---
+++ src/contracts/pods/EigenPodManagerV1.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.12;
+
+import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import "@openzeppelin/contracts/proxy/beacon/IBeacon.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
+
+import "../interfaces/IEigenPodManagerV1.sol";
+import "../interfaces/IETHPOSDeposit.sol";
+import "../interfaces/IEigenPodV1.sol";
+
+import "../permissions/Pausable.sol";
+import "./EigenPodPausingConstants.sol";
+
+/**
+ * @title The contract used for creating and managing EigenPods
+ * @author Layr Labs, Inc.
+ * @notice The main functionalities are:
+ * - creating EigenPods
+ * - staking for new validators on EigenPods
+ * - keeping track of the balances of all validators of EigenPods, and their stake in EigenLayer
+ * - withdrawing eth when withdrawals are initiated
+ */
+contract EigenPodManager is Initializable, OwnableUpgradeable, Pausable, IEigenPodManager, EigenPodPausingConstants {
+    /// @notice The ETH2 Deposit Contract
+    IETHPOSDeposit public immutable ethPOS;
+    
+    /// @notice Beacon proxy to which the EigenPods point
+    IBeacon public immutable eigenPodBeacon;
+    
+    /// @notice Pod owner to deployed EigenPod address
+    mapping(address => IEigenPod) internal _podByOwner;
+
+    /// @notice Emitted to notify the deployment of an EigenPod
+    event PodDeployed(address indexed eigenPod, address indexed podOwner);
+
+    modifier onlyEigenPod(address podOwner) {
+        require(address(getPod(podOwner)) == msg.sender, "EigenPodManager.onlyEigenPod: not a pod");
+        _;
+    }
+
+    constructor(IETHPOSDeposit _ethPOS, IBeacon _eigenPodBeacon) {
+        ethPOS = _ethPOS;
+        eigenPodBeacon = _eigenPodBeacon;
+        _disableInitializers();
+    }
+
+    function initialize(
+        address initialOwner,
+        IPauserRegistry _pauserRegistry,
+        uint256 _initPausedStatus
+    ) public initializer {
+        _transferOwnership(initialOwner);
+        _initializePauser(_pauserRegistry, _initPausedStatus);
+    }
+
+    /**
+     * @notice Creates an EigenPod for the sender.
+     * @dev Function will revert if the `msg.sender` already has an EigenPod.
+     */
+    function createPod() external {
+        require(!hasPod(msg.sender), "EigenPodManager.createPod: Sender already has a pod");
+        // deploy a pod if the sender doesn't have one already
+        _deployPod();
+    }
+
+    /**
+     * @notice Stakes for a new beacon chain validator on the sender's EigenPod. 
+     * Also creates an EigenPod for the sender if they don't have one already.
+     * @param pubkey The 48 bytes public key of the beacon chain validator.
+     * @param signature The validator's signature of the deposit data.
+     * @param depositDataRoot The root/hash of the deposit data for the validator's deposit.
+     */
+    function stake(bytes calldata pubkey, bytes calldata signature, bytes32 depositDataRoot) external payable {
+        IEigenPod pod = getPod(msg.sender);
+        if(!hasPod(msg.sender)) {
+            //deploy a pod if the sender doesn't have one already
+            pod = _deployPod();
+        }
+        pod.stake{value: msg.value}(pubkey, signature, depositDataRoot);
+    }
+
+    // INTERNAL FUNCTIONS
+    function _deployPod() internal onlyWhenNotPaused(PAUSED_NEW_EIGENPODS) returns (IEigenPod) {
+        IEigenPod pod = 
+            IEigenPod(
+                Create2.deploy(
+                    0, 
+                    bytes32(uint256(uint160(msg.sender))), 
+                    // set the beacon address to the eigenPodBeacon and initialize it
+                    abi.encodePacked(
+                        type(BeaconProxy).creationCode, 
+                        abi.encode(eigenPodBeacon, abi.encodeWithSelector(IEigenPod.initialize.selector, IEigenPodManager(address(this)), msg.sender))
+                    )
+                )
+            );
+        emit PodDeployed(address(pod), msg.sender);
+        return pod;
+    }
+
+    // VIEW FUNCTIONS
+    /// @notice Returns the address of the `podOwner`'s EigenPod (whether it is deployed yet or not).
+    function getPod(address podOwner) public view returns (IEigenPod) {
+        IEigenPod pod = _podByOwner[podOwner];
+        // if pod does not exist already, calculate what its address *will be* once it is deployed
+        if (address(pod) == address(0)) {
+            pod = IEigenPod(
+                Create2.computeAddress(
+                    bytes32(uint256(uint160(podOwner))), //salt
+                    keccak256(abi.encodePacked(
+                        type(BeaconProxy).creationCode, 
+                        abi.encode(eigenPodBeacon, abi.encodeWithSelector(IEigenPod.initialize.selector, IEigenPodManager(address(this)), podOwner))
+                    )) //bytecode
+                ));
+        }
+        return pod;
+    }
+
+    /// @notice Returns 'true' if the `podOwner` has created an EigenPod, and 'false' otherwise.
+    function hasPod(address podOwner) public view returns (bool) {
+        return address(_podByOwner[podOwner]) != address(0);
+    }
+}
---
+++ src/contracts/pods/EigenPodV1.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.12;
+
+import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin-upgrades/contracts/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin-upgrades/contracts/utils/AddressUpgradeable.sol";
+
+import "../interfaces/IETHPOSDeposit.sol";
+import "../interfaces/IEigenPodManagerV1.sol";
+import "../interfaces/IEigenPodV1.sol";
+import "../interfaces/IEigenPodPaymentEscrow.sol";
+import "../interfaces/IPausable.sol";
+
+import "./EigenPodPausingConstants.sol";
+
+import "forge-std/Test.sol";
+
+/**
+ * @title The implementation contract used for restaking beacon chain ETH on EigenLayer 
+ * @author Layr Labs, Inc.
+ * @notice The main functionalities are:
+ * - creating new ETH validators with their withdrawal credentials pointed to this contract
+ * - proving from beacon chain state roots that withdrawal credentials are pointed to this contract
+ * - proving from beacon chain state roots the balances of ETH validators with their withdrawal credentials
+ *   pointed to this contract
+ * - updating aggregate balances in the EigenPodManager
+ * - withdrawing eth when withdrawals are initiated
+ * @dev Note that all beacon chain balances are stored as gwei within the beacon chain datastructures. We choose
+ *   to account balances in terms of gwei in the EigenPod contract and convert to wei when making calls to other contracts
+ */
+contract EigenPod is IEigenPod, Initializable, ReentrancyGuardUpgradeable, EigenPodPausingConstants {
+    uint256 internal constant GWEI_TO_WEI = 1e9;
+
+    /// @notice This is the beacon chain deposit contract
+    IETHPOSDeposit internal immutable ethPOS;
+
+    /// @notice Escrow contract used for payment routing, to provide an extra "safety net"
+    IEigenPodPaymentEscrow immutable public eigenPodPaymentEscrow;
+
+    /// @notice The single EigenPodManager for EigenLayer
+    IEigenPodManager public eigenPodManager;
+
+    /// @notice The owner of this EigenPod
+    address public podOwner;
+
+    /// @notice The latest block number at which the pod owner withdrew the balance of the pod
+    uint32 public latestWithdrawalBlock;
+
+    /// @notice Emitted when an ETH validator stakes via this eigenPod
+    event EigenPodStaked(bytes pubkey);
+
+    modifier onlyEigenPodManager {
+        require(msg.sender == address(eigenPodManager), "EigenPod.onlyEigenPodManager: not eigenPodManager");
+        _;
+    }
+
+    modifier onlyEigenPodOwner {
+        require(msg.sender == podOwner, "EigenPod.onlyEigenPodOwner: not podOwner");
+        _;
+    }
+
+    /**
+     * @notice Based on 'Pausable' code, but uses the storage of the EigenPodManager instead of this contract. This construction
+     * is necessary for enabling pausing all EigenPods at the same time (due to EigenPods being Beacon Proxies).
+     * Modifier throws if the `indexed`th bit of `_paused` in the EigenPodManager is 1, i.e. if the `index`th pause switch is flipped.
+     */
+    modifier onlyWhenNotPaused(uint8 index) {
+        require(!IPausable(address(eigenPodManager)).paused(index), "EigenPod.onlyWhenNotPaused: index is paused in EigenPodManager");
+        _;
+    }
+
+    constructor(
+        IETHPOSDeposit _ethPOS,
+        IEigenPodPaymentEscrow _eigenPodPaymentEscrow
+    ) {
+        ethPOS = _ethPOS;
+        eigenPodPaymentEscrow = _eigenPodPaymentEscrow;
+        _disableInitializers();
+    }
+
+    /// @notice Used to initialize the pointers to contracts crucial to the pod's functionality, in beacon proxy construction from EigenPodManager
+    function initialize(IEigenPodManager _eigenPodManager, address _podOwner) external initializer {
+        require(_podOwner != address(0), "EigenPod.initialize: podOwner cannot be zero address");
+        eigenPodManager = _eigenPodManager;
+        podOwner = _podOwner;
+    }
+
+    /// @notice Called by EigenPodManager when the owner wants to create another ETH validator.
+    function stake(bytes calldata pubkey, bytes calldata signature, bytes32 depositDataRoot) external payable onlyEigenPodManager {
+        // stake on ethpos
+        require(msg.value == 32 ether, "EigenPod.stake: must initially stake for any validator with 32 ether");
+        ethPOS.deposit{value : 32 ether}(pubkey, _podWithdrawalCredentials(), signature, depositDataRoot);
+        emit EigenPodStaked(pubkey);
+    }
+
+    /// @notice Called by the pod owner to withdraw the balance of the pod
+    function withdraw() external onlyEigenPodOwner {
+        latestWithdrawalBlock = uint32(block.number);
+        _sendETH(podOwner, address(this).balance);
+    }
+
+    // INTERNAL FUNCTIONS
+
+    function _podWithdrawalCredentials() internal view returns(bytes memory) {
+        return abi.encodePacked(bytes1(uint8(1)), bytes11(0), address(this));
+    }
+
+    function _sendETH(address recipient, uint256 amountWei) internal {
+        eigenPodPaymentEscrow.createPayment{value: amountWei}(podOwner, recipient);
+    }
+}
