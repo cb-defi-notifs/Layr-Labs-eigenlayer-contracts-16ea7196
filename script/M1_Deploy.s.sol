3449fb1d33e365887c4a01c8735473f435d21e96
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-03-03 17:38:44 -0800

    clean up file

--- script/M1_Deploy.s.sol
+++ script/M1_Deploy.s.sol
@@ -247,14 +247,7 @@ contract Deployer_M1 is Script, Owners {
         _checkPauserInitializations();
         vm.stopBroadcast();
 
-        // TODO: write to file using vm.writeJSON -- see https://github.com/foundry-rs/foundry/pull/3595 or https://book.getfoundry.sh/cheatcodes/serialize-json
         string memory parent_object = "parent object";
-        // vm.serializeAddress(parent_object, "investmentManager", address(investmentManager));
-        // vm.serializeAddress(parent_object, "delegation", address(delegation));
-
-        // string memory serialized_addresses = "serialized addresses";
-        // vm.serializeAddress(serialized_addresses, "investmentManager", address(investmentManager));
-        // vm.serializeAddress(serialized_addresses, "delegation", address(delegation));
 
         string memory deployed_addresses = "deployed addresses";
         vm.serializeAddress(deployed_addresses, "investmentManager", address(investmentManager));
@@ -267,30 +260,6 @@ contract Deployer_M1 is Script, Owners {
         vm.serializeString(parent_object, deployed_addresses, output);
         string memory finalJson = vm.serializeString(parent_object, parameters, output_2);
         vm.writeJson(finalJson, "script/output/M1_deployment_data.json");
-
-
-
-        // // TODO: write to file using vm.writeJSON -- see https://github.com/foundry-rs/foundry/pull/3595 or https://book.getfoundry.sh/cheatcodes/serialize-json
-        // string memory parent_object = "some key";
-        // string memory deployed_addresses = "some other key";
-        // string memory output = vm.serializeAddress(object_2, "investmentManager", address(investmentManager));
-        // emit log_named_string("output", output);
-        // vm.serializeAddress(output, "delegation", address(delegation));
-        // emit log_named_string("output", output);
-        // emit log_named_string("object_2", object_2);
-
-        // // IMPORTANT: This works because `serializeString` first tries to interpret `output` as
-        // //   a stringified JSON object. If the parsing fails, then it treats it as a normal
-        // //   string instead.
-        // //   For instance, an `output` equal to '{ "ok": "asd" }' will produce an object, but
-        // //   an output equal to '"ok": "asd" }' will just produce a normal string.
-        // string memory finalJson = vm.serializeString(parent_object, "deployed addresses", output);
-
-        // emit log_named_string("finalJson", finalJson);
-        // vm.writeJson(finalJson, "script/output/M1_deployment_data.json");
-        // // vm.writeFile("data/investmentManager.addr", vm.toString(address(investmentManager)));
-        // // vm.writeFile("data/delegation.addr", vm.toString(address(delegation)));
-        // // vm.writeFile("data/slasher.addr", vm.toString(address(slasher)));
     }
 
     function _verifyContractsPointAtOneAnother(
