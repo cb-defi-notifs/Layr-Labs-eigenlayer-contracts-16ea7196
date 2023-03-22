52922210d41d3d681f97fc4ccda8b6696893617a
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-03-22 14:30:14 -0700

    change SPDX identifier (UNLICENSED => BUSL-1.1)

--- src/test/unit/StrategyBaseUnit.t.sol
+++ src/test/unit/StrategyBaseUnit.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
